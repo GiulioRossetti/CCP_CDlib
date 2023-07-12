67b108872a51574104a03e0cd53f4941ac19a305
dcore <dcore94@users.noreply.github.com>
2023-07-12 16:26:09 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -2,27 +2,21 @@ import networkx as nx
 from cdlib import algorithms as al
 from cdlib import readwrite as io
 import argparse, sys, json
-import configparser;
-
-conf = configparser.ConfigParser()
-conf.read("config.cfg")
 
 parser=argparse.ArgumentParser()
-
-# parser.add_argument("--network", help="Network edgelist filename")
 parser.add_argument("--alg", help="Community detection algorithm")
+
+#parser.add_argument("--network", help="Network edgelist filename")
 #parser.add_argument("--params", help="dictionary of parameters for the algorithm", default="{}")
 
 args=parser.parse_args()
 
 print(args)
-print(conf["default"])
-#print(args.params)
-#params = json.loads(args.params, parse_float=float, parse_int=int)
+params = json.loads(open("params.json"))
+print(params)
 
 g = nx.read_edgelist("network.csv", delimiter=",")
 
 alg = getattr(al, args.alg)
-#coms = alg(g, **params)
-coms = alg(g, **conf["default"])
+coms = alg(g, **params)
 io.write_community_json(coms, "communities.json")
